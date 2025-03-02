8575f680a77ed17b2fb51a18f91570d5014a2546
Lijo Thomas <lijothomas7777777@gmail.com>
2025-03-03 02:06:38 +0530

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-def homepage(request):
-    #return HttpResponse("hello world !. i am home ...")
-    return render(request,'home.html')
-def about(request):
-    #return HttpResponse("this is my about page ")
-     return render(request,'about.html')
